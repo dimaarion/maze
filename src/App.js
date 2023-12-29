baea7c7116efc6dad637b25f69cd210b7f666bfc
dimaarion <dimaarion@gmail.com>
2023-12-29 07:42:51 +0200

    dimaarion/maze_2

--- src/App.js
+++ src/App.js
@@ -3,12 +3,32 @@ import Level from './components/Level';
 import scena_1 from "./asset/scena/scena.json";
 import scena_2 from "./asset/scena/scena2.json";
 import scena_3 from "./asset/scena/scena3.json"
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [seconds, setSeconds] = useState(1);
+  const [load, setLoad] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+     //setSeconds(seconds => seconds + 1);
+      console.log(document.getElementsByClassName("App")[0].style.display = "none")
+      if(document.getElementsByTagName("canvas")[0]){
+       console.log(document.getElementsByClassName("App")[0].style.display = "fixed")
+      }else{
+      
+      }
+
+    }, 10);
+    // очистка интервала
+    return () => clearInterval(timer);
+  });
+
+  
   return (
     <div className="App">
-          <Level bg={[
+         <Level bg={[
             { scena: scena_1, level: 1, id: [0], bg: "./img/Tiles/bg1.png", img: ["./img/Tiles/tiles.png"] },
             { scena: scena_2, level: 2, id: [0], bg: "./img/Tiles/bg1.png", img: ["./img/Tiles/tiles.png"] },
             { scena: scena_3, level: 3, id: [0], bg: "./img/Tiles/bg1.png", img: ["./img/Tiles/tiles.png"] }
