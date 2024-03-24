0ca5318c43bf87571060e4c8e9ac1d65c85db9ff
dimaarion <dimaarion@gmail.com>
2024-03-24 19:59:48 +0200

    dimaarion/maze_2

---
+++ public/asset/scena/level.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="level" tilewidth="50" tileheight="50" tilecount="3600" columns="60">
+ <image source="../../img/Tiles/level.png" width="3000" height="3000"/>
+ <tile id="0" type="www">
+  <properties>
+   <property name="sxss" value="asxas"/>
+  </properties>
+ </tile>
+ <tile id="370">
+  <properties>
+   <property name="test" value="test"/>
+  </properties>
+ </tile>
+</tileset>
